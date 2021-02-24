f4572f24c947c0b66d7ff248f7a6fbd613d4b4b0
rholgate_turnto <rholgate@turnto.com>
2021-02-24 11:47:02 -0800

    remove non-pipelines content

--- cartridges/int_turnto_sfra_v5/cartridge/models/product/decorators/ratingsturnto.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (object, apiProduct) {
-
-    Object.defineProperty(object, 'starRating', {
-        enumerable: true,
-        value: (Object.prototype.hasOwnProperty.call(apiProduct.custom, 'turntoAverageRating') && apiProduct.custom.turntoAverageRating) ? apiProduct.custom.turntoAverageRating : 0
-    });
-};
